00672f4b139169ffe70b0882c4eac632790a2dc6
zebi <zebi@eng.ucsd.edu>
2018-01-11 20:39:09 -0800

    Modify the program from #5 (still on branch assignment1-interrupt) to use interrupts. You’ll handle input interrupts from the button and ultrasonic sensor. This may require rewiring your breadboard circuits depending on which inputs you’re using.

--- Assignment_1/main.cpp
+++ Assignment_1/main.cpp
@@ -2,32 +2,33 @@
 #include "ultrasonic.h"
 
 Serial pc(USBTX, USBRX); // tx, rx
-DigitalIn pb(p12);
-Serial pc(USBTX, USBRX);
+InterruptIn pb(p12);
+DigitalOut flash(LED4);
 
 void dist(int distance) //this needs to be here...... check why
 {
     
 }
- 
+
 ultrasonic mu(p8, p9, .1, 1, &dist);
 
+void flip() {
+    wait(0.001);
+    if (pb.read()==0){
+        long distance = mu.getCurrentDistance();
+        printf("Distance: %d\r\n", distance);
+        wait(0.1);
+    }
+}
+ 
 int main() {
     pb.mode(PullUp);
-	mu.startUpdates();//start measuring the distance
-    int current = pb.read();
+    // Delay for initial pullup to take effect
+    wait(.001);
+    pb.fall(&flip);
+    mu.startUpdates();//start measuring the distance
     while(1) {
-        if (pb.read()!= current){
-            wait(0.001);
-            if (pb.read()!=current){
-                current=pb.read();
-                if (current == 0){
-                    mu.checkDistance();
-					long distance = mu.getCurrentDistance();
-					printf("Distance: %d\r\n", distance);
-					wait(0.1);
-                }
-            }
-		}
+        flash = !flash;
+        wait(0.25);
     }
 }
