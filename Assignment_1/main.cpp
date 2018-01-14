6808b281d99e43b1a9871dc496dbb8d08516229b
Benjamin Hobbs <bhobbs@eng.ucsd.edu>
2018-01-14 13:23:53 -0800

    On your breadboard, wire the motor controller to the MBED (power, 4 outputs). Write a program that takes input commands from the serial UART.  When the program receives ‘f’ it rotates the motor in clockwise direction. When the program receives ‘r’ it rotates the motor in counter-clockwise direction. Create branch assignment1-motor from master, commit your code to it, and push to Bitbucket.

---
+++ Assignment_1/main.cpp
@@ -0,0 +1,84 @@
+#include "mbed.h"
+
+Serial pc(USBTX, USBRX); // Define serial TX RX
+
+DigitalOut PWM1(p5);
+DigitalOut PWM2(p6);
+DigitalOut PWM3(p7);
+DigitalOut PWM4(p8);
+
+
+
+
+int main()
+{
+    char c = pc.getc();
+    int count=0;
+    int count1=0;
+    pc.printf("ready \r\n");
+    while(1) {
+        // specify period first
+        if(pc.readable()) {
+            c = pc.getc();
+        }
+        if((c == 'f')) { //if f presed clock wise
+            count1++;
+            pc.printf(" %d ",count1);
+
+            PWM4.write(1);
+            wait(.01);
+            PWM4.write(0);
+
+            wait(.01);
+
+            PWM3.write(1);
+            wait(.01);
+            PWM3.write(0);
+
+            wait(.01);
+
+            PWM2.write(1);
+            wait(.01);
+            PWM2.write(0);
+
+            wait(.01);
+
+            PWM1.write(1);
+            wait(.01);
+            PWM1.write(0);
+        }
+
+
+//if r pressed, counter clock
+        if((c == 'r')) {
+
+            count++;
+            pc.printf(" %d ",count);
+
+
+            PWM1.write(1);
+            wait(.01);
+            PWM1.write(0);
+
+            wait(.01);
+
+            PWM2.write(1);
+            wait(.01);
+            PWM2.write(0);
+
+            wait(.01);
+
+            PWM3.write(1);
+            wait(.01);
+            PWM3.write(0);
+
+            wait(.01);
+
+            PWM4.write(1);
+            wait(.01);
+            PWM4.write(0);
+        }
+
+
+    }
+}
