a6c0041ab50c9257104b472e74cc73039023a05b
zebi <zebi@eng.ucsd.edu>
2018-01-18 19:29:46 -0800

    You will be creating a small radar station. Begin by mounting the small breadboard to the motor platform and wire the ultrasonic on it. Write a program that sweeps the motor 180° clockwise and then counter-clockwise (ie. back and forth) in a continuous and smooth motion. It should perform a complete sweep (left-right-left) every 8 seconds. Every .5 seconds perform a ping on the ultrasonic sensor. If an object is observed print the current angle of your platform (degrees from center) and distance to object on the serial UART. Use the button input to begin/pause radar operation.

---
+++ Assignment_1/main.cpp
@@ -0,0 +1,43 @@
+#include "mbed.h"
+#include "sMotor.h"
+
+InterruptIn pb(p12);
+
+Serial pc(USBTX, USBRX);
+sMotor motor(p14, p15, p16, p17); // creates new stepper motor: IN1, IN2, IN3, IN4
+
+Ticker measure;
+
+int step_speed = 2000 ; // set default motor speed
+int numstep = 540 ; // defines full turn of 360 degree
+//you might want to calibrate this value according to your motor
+
+void checkmeasure() {
+    measuredist = 1;
+}
+
+void flip() {
+    wait(0.001);
+    if (pb.read()==0){
+        if (pause == false){
+            measure.detach();
+        }
+        if (pause == true){
+            measure.attach(&checkmeasure, 0.5);
+        }
+        pause = !pause;
+    }
+}
+
+int main()
+{
+    measure.attach(&checkmeasure, 0.5);
+    pb.mode(PullUp);
+    // Delay for initial pullup to take effect
+    wait(.001);
+    pb.fall(&flip);
+    while (1) {
+        motor.step(numstep/2,0,step_speed);
+        motor.step(numstep/2,1,step_speed);
+    }
+}
