38b1d4bc4224eadafaa1df88f49ac2189c77e2ad
zbi <zbi@qti.qualcomm.com>
2018-02-01 22:10:24 -0800

    Wire the compass / accelerometer board to your radar platform (place it on the small breadboard with the ultrasonic sensor). Update your radar station program to display its current scan heading (N, NW, W, etc) at the top right of the graphic display. This should update as it rotates.

--- Assignment_2/main.cpp
+++ Assignment_2/main.cpp
@@ -3,8 +3,14 @@
 #include "ultrasonic.h"
 #include "circBuffer.h"
 #include "Adafruit_SSD1306.h"
+#include "LSM303DLHC.h"
 
 #define PI 3.1416
+#define Rad2Deg 57.2958
+
+LSM303DLHC compass(p9, p10);//compass
+
+char *cardDir;
 
 // an I2C sub-class that provides a constructed default
 class I2CPreInit : public I2C
@@ -50,6 +56,7 @@ void drawRadarPin()
     x1 = 64 + (64 * cos(rad));
     y1 = 64 - (64 * sin(rad));
     gOled2.clearDisplay();
+    gOled2.printf("                  %s\r",cardDir);
     gOled2.drawCircle(64, 64, 63, WHITE);
     gOled2.drawCircle(64, 64, 47, WHITE);
     gOled2.drawCircle(64, 64, 31, WHITE);
@@ -77,50 +84,6 @@ void printDistance(void)
         foundx[numberFound]=x1;
         foundy[numberFound]=y1;
         numberFound++;
-    }
-}
-
-void dist(int distance)
-{
-    sensorDistance = distance;
-    rad = motorpositionstep * PI/270;
-    if (circBuffPush(&buffP2, &printDistance)) {
-        pc.printf("P2 buffer full\r\n");
-    }
-    if (circBuffPush(&buffP3, &drawRadarPin)) {
-        pc.printf("P3 buffer full\r\n");
-    }
-}
-
-ultrasonic mu(p11, p12, .1, 1, &dist);
-
-void checkmeasure()
-{
-    mu.checkDistance();
-}
-
-void buttonAction()
-{
-    if (pause == true) {
-        measure.detach();
-        //pc.printf("Paused\r\n");
-        if (circBuffPush(&buffP1, &buttonAction)) {
-            pc.printf("P1 buffer full\r\n");
-        }
-    }
-    if (pause == false) {
-        measure.attach(&checkmeasure, 0.5);
-    }
-}
-
-void flipISR()
-{
-    wait(0.01);
-    if (pb.read()==0) {
-        pause=!pause;
-        if (circBuffPush(&buffP1, &buttonAction)) {
-            pc.printf("P1 buffer full\r\n");
-        }
     }
 }
 
@@ -160,6 +123,92 @@ void moveMotor()
     }
 }
 
+void readCompass()
+{
+    int magReading[3] = {0};
+    double x,y;
+    double deg;
+    compass.readMag(magReading);
+
+    x = magReading[0];
+    y = magReading[1];
+
+    deg = atan2(y,x);
+    deg *= Rad2Deg;
+
+    if (deg <0) deg = 360.0 + deg;
+    //if (deg > 360.0) deg = deg - 360.0 ;
+
+    if (337.5 <= deg && deg < 360) {
+        cardDir = "N";
+    } else if (0 <= deg && deg < 22.5) {
+        cardDir = "N";
+    } else if (22.5 <= deg && deg < 67.5) {
+        cardDir = "NE";
+    } else if (67.5 <= deg && deg < 112.5) {
+        cardDir = "E";
+    } else if (112.5 <= deg && deg < 157.5) {
+        cardDir = "SE";
+    } else if (157.5 <= deg && deg < 202.5) {
+        cardDir = "S";
+    } else if (202.5 <= deg && deg < 247.5) {
+        cardDir = "SW";
+    } else if (247.5 <= deg && deg < 292.5) {
+        cardDir = "W";
+    } else if (292.5 <= deg && deg < 337.5) {
+        cardDir = "NW";
+    } else {
+        pc.printf("something went wrong!\r\n");
+    }
+}
+
+void dist(int distance)
+{
+    sensorDistance = distance;
+    rad = motorpositionstep * PI/270;
+    if (circBuffPush(&buffP2, &printDistance)) {
+        pc.printf("P2 buffer full\r\n");
+    }
+    if (circBuffPush(&buffP2, &readCompass)) {
+        pc.printf("P2 buffer full\r\n");
+    }
+    if (circBuffPush(&buffP3, &drawRadarPin)) {
+        pc.printf("P3 buffer full\r\n");
+    }
+}
+
+ultrasonic mu(p11, p12, .1, 1, &dist);
+
+void checkmeasure()
+{
+    mu.checkDistance();
+}
+
+void buttonAction()
+{
+    if (pause == true) {
+        measure.detach();
+        //pc.printf("Paused\r\n");
+        if (circBuffPush(&buffP1, &buttonAction)) {
+            pc.printf("P1 buffer full\r\n");
+        }
+    }
+    if (pause == false) {
+        measure.attach(&checkmeasure, 0.5);
+    }
+}
+
+void flipISR()
+{
+    wait(0.01);
+    if (pb.read()==0) {
+        pause=!pause;
+        if (circBuffPush(&buffP1, &buttonAction)) {
+            pc.printf("P1 buffer full\r\n");
+        }
+    }
+}
+
 int main()
 {
     fooptr runningPtr;
@@ -167,6 +216,10 @@ int main()
     mu.startUpdates();//start measuring the distance
 
     measure.attach(&checkmeasure, 0.5);
+
+    compass.init();
+    compass.setOffset(29.50, -0.50, 4.00); // example calibration
+    compass.setScale(1.00, 1.03, 1.21);    // example calibration
 
     pb.mode(PullUp);
     // Delay for initial pullup to take effect
